2a11bceb5e69c9fb46801611e6f8f3d998bfca86
Matthew Phillips <phillipsm2@gmail.com>
2012-03-05 10:30:07 -0500

    Initial commit.

---
+++ src/Button.js
@@ -0,0 +1,44 @@
+var Button = Backbone.View.extend({
+
+  tagName: 'button',
+
+  className: 'agate-button',
+
+  text: '',
+
+  events: {
+    'mousedown': 'down',
+    'touchstart': 'down',
+    'mousemove': 'move',
+    'touchmove': 'move',
+    'mouseup': 'up',
+    'touchend': 'up'
+  },
+
+  initialize: function() {
+    if(this.options && this.options.text)
+      this.text = this.options.text;
+  },
+
+  render: function() {
+    this.el.textContent = this.text;
+    return this;
+  },
+
+  down: function() {
+
+  },
+
+  move: function() {
+
+  },
+
+  up: function() {
+
+  }
+
+});
+
+var btn = new Button({ text: 'Hello' });
+var el = btn.render().el;
+document.body.appendChild(el);
