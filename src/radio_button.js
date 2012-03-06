14f37beab81ce6daeacb6fb87cae275cda251a45
Matthew Phillips <phillipsm2@gmail.com>
2012-03-06 13:52:41 -0500

    Cleaner syntax for the radio button.

--- src/radio_button.js
+++ src/radio_button.js
@@ -1,18 +1,3 @@
 var RadioButtonView = Backbone.View.extend({
-
-  tagName: 'button',
-
-  className: 'agate-radiobutton',
-
-  model: new Button(),
-
-  initialize: function() {
-    if(!(this.model instanceof Button))
-      this.model = new Button(this.model);
-  },
-
-  render: function() {
-    this.el.textContent = this.model.get('text');
-    return this;
-  }
+  className: 'agate-radiobutton'
 });
