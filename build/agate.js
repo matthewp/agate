91b66d2350f9685c05d3108e1b104392fde01893
Matthew Phillips <phillipsm2@gmail.com>
2012-03-06 22:39:36 -0500

    Radio group now changing on mouseup, touchend.

--- build/agate.js
+++ build/agate.js
@@ -41,6 +41,11 @@ var RadioGroupView = Backbone.View.extend({
   
   buttons: [],
 
+  events: {
+    'mouseup button': 'up',
+    'touchend button': 'up'
+  },
+
   initialize: function() {
     var buttons = this.options.buttons;
     if(buttons && buttons.length > 0) {
@@ -57,6 +62,26 @@ var RadioGroupView = Backbone.View.extend({
     });
 
     return self;
+  },
+
+  up: function(e) {
+    var el = e.target,
+        classes = el.className.split(' '),
+        active = classes.indexOf('active') !== -1;
+    
+    if(!active) {
+      
+      this.options.buttons.forEach(function(btn) {
+        var cs = btn.el.className.split(' ');
+        
+        btn.model.set('active', false);
+        btn.el.className = _.without(cs, 'active').join(' ');
+      });
+
+      classes.push('active');
+      el.className = classes.join(' ');
+
+    }
   }
 
 });
--- src/radio_group.js
+++ src/radio_group.js
@@ -2,6 +2,11 @@ var RadioGroupView = Backbone.View.extend({
   
   buttons: [],
 
+  events: {
+    'mouseup button': 'up',
+    'touchend button': 'up'
+  },
+
   initialize: function() {
     var buttons = this.options.buttons;
     if(buttons && buttons.length > 0) {
@@ -18,6 +23,26 @@ var RadioGroupView = Backbone.View.extend({
     });
 
     return self;
+  },
+
+  up: function(e) {
+    var el = e.target,
+        classes = el.className.split(' '),
+        active = classes.indexOf('active') !== -1;
+    
+    if(!active) {
+      
+      this.options.buttons.forEach(function(btn) {
+        var cs = btn.el.className.split(' ');
+        
+        btn.model.set('active', false);
+        btn.el.className = _.without(cs, 'active').join(' ');
+      });
+
+      classes.push('active');
+      el.className = classes.join(' ');
+
+    }
   }
 
 });
